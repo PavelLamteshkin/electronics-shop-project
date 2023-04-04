cc156f6a427c7a13b54782dc6e183d34dd000460
MummyHouse <lamteshkin@gmail.com>
2023-04-04 11:22:34 +0300

    items.py доработан проведены тесты - покрытие 100%

--- src/item.py
+++ src/item.py
@@ -8,23 +8,23 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
-        :param name: Название товара.
-        :param price: Цена за единицу товара.
-        :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        self.all.append(Item)
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
-        :return: Общая стоимость товара.
         """
-        pass
+        return self.quantity * self.price
+
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price * self.pay_rate
+        return self.price
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item1():
+    return Item("Наушники", 1000, 10)
+
+
+def test_calculate_total_price(item1):
+    assert Item.calculate_total_price(item1) == 10000
+
+
+def test_apply_discount(item1):
+    assert Item.apply_discount(item1) == 1000
