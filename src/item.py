ef74b306639aedcc3c089b9b987d31b620894c61
MummyHouse <lamteshkin@gmail.com>
2023-04-11 16:37:32 +0300

    Подготовка к ДЗ2

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+import os.path
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -9,11 +12,24 @@ class Item:
         """
         Создание экземпляра класса item.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
+        # Item.all.append(self)
 
-        self.all.append(Item)
+
+    @property
+    def name(self):
+        return self.__name
+
+
+    @name.setter
+    def name(self, name):
+        if len(name) > 10:
+            raise Exception('Длина наименования товара превышает 10 символов')
+        else:
+            self.__name = name
+
 
     def calculate_total_price(self) -> float:
         """
@@ -26,5 +42,33 @@ class Item:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        self.price = self.price * self.pay_rate
-        return self.price
+        return self.price * self.pay_rate
+
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        """
+        метод инициализирующий экземпляры класса Item данными из файла src/items.csv
+        """
+        # path = os.path.abspath('items.csv')
+        # print(os.path.isdir(r'C:\Users\MummyHouse\PycharmProjects\electronics-shop-project\src\items.csv'))
+        path = r'C:\Users\MummyHouse\PycharmProjects\electronics-shop-project\src\items.csv'
+        with open(path) as csvfile:
+            reader = csv.DictReader(csvfile, delimiter = ',')
+            for row in reader:
+                name = row['name']
+                price = row['price']
+                quantity = row['quantity']
+                item = [name, price, quantity]
+                Item.all.append(item)
+
+
+    @staticmethod
+    def string_to_number(num: str):
+        """
+        метод возвращающий число из числа-строки
+        """
+        return round(int(float(num)))
+
+# print(Item.instantiate_from_csv())
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,9 @@
 import pytest
 from src.item import Item
 
+item = Item("Ноутбук", 100000, 5)
+assert isinstance(item, Item)
+
 
 @pytest.fixture
 def item1():
@@ -13,3 +16,18 @@ def test_calculate_total_price(item1):
 
 def test_apply_discount(item1):
     assert Item.apply_discount(item1) == 1000
+
+
+def test_string_to_number():
+    assert Item.string_to_number('7.5') == 7
+    assert Item.string_to_number('7') == 7
+
+
+# def test_name():
+#     item2 = Item("Супернаушники", 1000, 10)
+#     assert Item.name(item2.name) == 'Длина наименования товара превышает 10 символов'
+
+
+def test_instantiate_from_csv():
+    assert Item.instantiate_from_csv() == None
+
